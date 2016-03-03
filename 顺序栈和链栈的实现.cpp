b5302f9e17e45c38d337419f938b4883d26fcef7
Li Jun <hexlul@users.noreply.github.com>
2016-03-03 11:47:14 +0800

    Update and rename 顺序栈的实现.cpp to 顺序栈和链栈的实现.cpp

--- 顺序栈和链栈的实现.cpp
+++ 顺序栈和链栈的实现.cpp
@@ -15,3 +15,24 @@ class SeqStack
       DataType data[StackSize];  //存放栈元素的数组
       int top;                  //栈顶指针，为栈顶元素在数组中的下标
 };
+
+//链栈的实现
+template <class DataType>
+class LinkStack
+{
+    public:
+       LinkStack() {top == NULL;}
+       ~LinkStack();
+       void Push(DataType x);
+       DataType Pop();
+       DataType GetTop() {if(top != NULL) return top -> data;}
+       int Empty() {top == NULL?return 1: return 0;}
+    private:
+       Node<DataType> * top;   //栈顶指针即链栈的头指针
+};
+//链栈的入栈算法Push
+template <class DataType>
+void LinkStack<DataType>::Push(DataType x)
+{
+    
+}
